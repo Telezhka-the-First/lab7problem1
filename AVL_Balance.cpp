a1c32aefb1d37202b6c6f887a7074fbea9085e8b
Ignatius <339093@niuitmo.ru>
2021-12-20 11:04:32 +0300

    Added recursive function for height count of an AVL-tree

--- AVL_Balance.cpp
+++ AVL_Balance.cpp
@@ -10,6 +10,14 @@ struct AVL_tree {
     int balance;
 };
 
+int Count_Height(AVL_tree tree[], int id) {
+    if (id == 0) return 0;
+    int left_height  = Count_Height(tree, tree[id].left)  + 1;
+    int right_height = Count_Height(tree, tree[id].right) + 1;
+    tree[id].balance = right_height - left_height;
+    return tree[id].balance > 0 ? right_height : left_height;
+}
+
 int main() {
 	ifstream foe("balance.in");
     ofstream ally("balance.out");
@@ -21,6 +29,8 @@ int main() {
     AVL_tree *tree = new AVL_tree[number * sizeof(AVL_tree)];
     
     for (int i = 1; i < number; i++) foe >> tree[i].id >> tree[i].left >> tree[i].right;
+	
+	Count_Height(tree, 1);
 	
 	foe.close();
     ally.close();
