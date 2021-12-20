b1fba796104c4c0878b83571798a8d24e4ac16a0
Ignatius <339093@niuitmo.ru>
2021-12-20 11:01:56 +0300

    Adding file operations

--- AVL_Balance.cpp
+++ AVL_Balance.cpp
@@ -15,5 +15,13 @@ int main() {
 
 	cout << "Hello world!";
 	
+	ifstream foe("balance.in");
+    ofstream ally("balance.out");
+
+    int number;
+    foe >> number;
+	
+	foe.close();
+    ally.close();
     return 0;
 }
