a192412454871879f7ba32d2d40134fbd4d764bd
Ignatius <UserMail@email.com>
2021-12-20 11:44:18 +0300

    Add AVL-tree

--- AVL_Balance.cpp
+++ AVL_Balance.cpp
@@ -3,12 +3,22 @@
 
 using namespace std;
 
+struct AVL_tree {
+    int id;
+    int left;
+    int right;
+    int balance;
+};
+
 int main() {
 	ifstream foe("balance.in");
     ofstream ally("balance.out");
 
     int number;
     foe >> number;
+	number++;
+
+    AVL_tree *tree = new AVL_tree[number * sizeof(AVL_tree)];
 	
 	foe.close();
     ally.close();
